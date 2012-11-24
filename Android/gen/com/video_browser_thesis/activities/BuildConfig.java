4120ef763733d61bf1d24f2eb767cfa284ac3bab
u4545909 <u4545909@anu.edu.au>
2012-11-23 16:35:02 -0800

    android files deleted for replacing with a new version, APK updated to show loading sign

--- Android/gen/com/video_browser_thesis/activities/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.video_browser_thesis.activities;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
